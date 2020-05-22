d2f66f8f75942500b4017980f6a202060b752d49
zhuganglie <pyrrhonianpig@gmail.com>
2020-05-23 01:44:18 +0800

    update

--- select.js
+++ select.js
@@ -1,3 +1,4 @@
+window.onload = () => {
 var filter0 = menu.filter(function(veg) {
   return veg.type == "素菜";
 });
@@ -12,28 +13,21 @@ function getFields(input, field) {
 }
 var veg = getFields(filter0, "name");
 var meat = getFields(filter1, "name");
-var list = [veg, meat];
-function getList() {
-  var sltType = document.form1.type;
-  var sltName = document.form1.name;
-  var typeName = list[sltType.selectedIndex - 1];
-  sltName.length = 1;
-  for (var i = 0; i <typeName.length; i++) {
-    sltName[i + 1] = new Option(typeName[i], typeName[i]);
-  };
+/*var list = [veg, meat];*/
+var list = veg.concat(meat);
+var select = document.getElementById("choice");
+for (var i = 0; i <list.length; i++) {
+  var opt = list[i];
+  var el = document.createElement("option");
+  el.textContent = opt;
+  el.value = opt;
+  select.appendChild(el);
 };
-/*$(document).ready(function() {
-  $('#name').on('change', function() {
-    var optionText = $("#name option:selected").text();
-    alert(optionText);
-  });
-});*/
-$("select")
-.change(function() {
-    var str = "";
-    $("select#name option:selected").each(function() {
-      str += $(this).text() + " ";
-    });
-    $("#display").text(str);
-  })
-  .trigger("change");
+}
+
+function printAll() {
+ var text = $('#choice option:selected').toArray().map(item => item.text).join('、');
+ document.getElementById('display').innerHTML = text;
+ }
+
+
